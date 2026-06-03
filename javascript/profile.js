02bb6314649217da02077195c6201f387c390d3c
Menna Alamin <Mennaelamin2@gmail.com>
2026-06-03 20:46:47 +0300

    frontend of pf

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded",function(){
 fetch("../data.json")//read json
 .then(hold=> hold.json()) //unlock w 22raha
 .then(data => //23ml b 2l data kaza
@@ -5,7 +6,40 @@ fetch("../data.json")//read json
  const readparam = new URLSearchParams(window.location.search); //hat 7tt ? f 2l url
  const id = parseInt(readparam.get("id")); //hat value 2l id w 7wloh int
  const user = data.users.find(u=>u.id === id);
+
+ if(user.image && user.image!='')
+ {
  document.getElementById("profileimg").src= user.image;
+
+
+const overlay = document.getElementById("certOverlay");
+const zoomed = document.getElementById("certZoomed");
+
+document.getElementById("profileimg").style.cursor = "pointer";
+document.getElementById("profileimg").addEventListener("click", () => {
+   zoomed.classList.add("pfp-zoomed");
+  zoomed.src = document.getElementById("profileimg").src;
+  overlay.classList.add("active");
+});
+
+overlay.addEventListener("click", () => {
+  overlay.classList.remove("active");
+    zoomed.classList.remove("pfp-zoomed");
+});
+ }
+ else
+ {
+   document.getElementById("profileimg").src= "../images/emptypf.jpg";
+ document.getElementById("profileimg").style.cursor="default";
+ }
+if(user.pfbg && user.pfbg!='')
+{
+document.getElementById("bgimage").src= user.pfbg;
+}
+else
+{
+   document.getElementById("bgimage").src= "../images/emptybg.jpg";
+}
  if (user.about2)
  {
  document.getElementById("theabout").textContent=user.about + " " + user.about2;
@@ -16,30 +50,7 @@ fetch("../data.json")//read json
  }
  document.getElementById("flname").textContent=user.firstname + " "+user.lastname;
 
-
- const placej = document.getElementById("jobarray");
- user.job.forEach(jobies =>
- {
- const newelement = document.createElement("span");
- newelement.textContent= jobies;
- newelement.classList.add("tag");
- newelement.classList.add("jobies-css");
- placej.appendChild(newelement);
- }
- );
-
- const placesk= document.getElementById("skillie");
- user.skills.forEach( sk=>
- {
-    const newelement = document.createElement("span");
- newelement.textContent=sk;
- newelement.classList.add("tag");
- newelement.classList.add("skills-css");
- placesk.appendChild(newelement);
-
- } );
-
-const placeww =document.getElementById("workedwith");
+ const placeww =document.getElementById("workedwith");
 if (user.Providedf )
 {
    user.Providedf.forEach( pid =>
@@ -48,18 +59,37 @@ if (user.Providedf )
    
    if (client)
    {
-      const newelement = document.createElement("span");
+      const newelement = document.createElement("a");
       newelement.textContent=client.firstname+" "+client.lastname;
       newelement.classList.add("tag");
       newelement.classList.add("client-css");
+      newelement.href = "profile.html?id="+client.id;
       placeww.appendChild(newelement);
    }
    });
 }
 
+ if (user.type==1){
+   document.getElementById("contskillie").style.display = "block";
+   document.getElementById("skillie").style.display = "block";
+ const placesk= document.getElementById("skillie");
+ user.skills.forEach( sk=>
+ {
+    const newelement = document.createElement("span");
+ newelement.textContent=sk;
+ newelement.classList.add("tag");
+ newelement.classList.add("skills-css");
+ placesk.appendChild(newelement);
+ 
+ } )
 
-
+ }
+ else{
+             document.getElementById("contskillie").style.display = "none";
+  document.getElementById("skillie").style.display = "none";
+ }
 
 }
-
 )
+.catch(err=>console.error ("fetch failed",err));
+});
